4131c71cc8ca34990c48287be6850aff13811b77
Azizb750 <Azizb750@gmail.com>
2018-08-14 14:51:20 +0000

    201808014

--- omni_hermes.mk
+++ omni_hermes.mk
@@ -1,4 +1,4 @@
-DEVICE_PATH := device/xiaomi/hermes
+DEVICE_PATH := device/xiaomi/herme
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
